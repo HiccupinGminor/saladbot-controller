1cd40e6fad3a1247d5cca23f324e414380dc64db
Chris Chung <hiccupingminor@gmail.com>
2017-09-21 20:37:06 -0700

    Check if separate resolution works better

--- camera/camera.py
+++ camera/camera.py
@@ -13,15 +13,15 @@ if CAMERA_MODULE is "v1.3":
 # dist_to_target - distance between lens and target (in mm)
 # size_of_target - one side of
 # returns: 4-tuple of (x, y, w, h)
-def frame_image(res_x, res_y, dist_to_target, size_of_target):
+def frame_image(sensor_width, sensor_height, dist_to_target, size_of_target):
     # what percentage of the image does the target occupy?
-    (u, v) = ( focal_length * size_of_target / dist_to_target ) / res_x , ( focal_length * size_of_target / dist_to_target ) / resolution_y
+    (u, v) = ( focal_length * size_of_target / dist_to_target ) / sensor_width , ( focal_length * size_of_target / dist_to_target ) / sensor_height
     print(u, v)
     return ((1 - u) / 2, (1 - v) / 2, u, v)
 
-resolution_x = 2952
-resolution_y = 1944
+resolution_x = 1920
+resolution_y = 1080
 
 camera.resolution = (resolution_x, resolution_y)
-camera.zoom = frame_image(resolution_x, resolution_y, DIST_FROM_LENSE_TO_GROUND, SIZE_OF_TARGET_ZONE)
+camera.zoom = frame_image(sensor_width, sensor_height, DIST_FROM_LENSE_TO_GROUND, SIZE_OF_TARGET_ZONE)
 camera.capture('../foo.jpg')
