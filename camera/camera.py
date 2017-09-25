a5cce21823ee0e6067d28afaff6467749d50d3c1
Chris Chung <hiccupingminor@gmail.com>
2017-09-25 10:57:03 -0700

    Add detector script

--- camera/camera.py
+++ camera/camera.py
@@ -5,10 +5,16 @@ DIST_FROM_LENSE_TO_GROUND = 39.5
 SIZE_OF_TARGET_ZONE = 10
 CAMERA_MODULE = "v1.3"
 
+resolution_x = 1920
+resolution_y = 1080
+pixel_width = .0014
+
+# All measurements in millimeters
 if CAMERA_MODULE is "v1.3":
-    sensor_width = 3.76
-    sensor_height = 2.74
-    focal_length = 3.60 # millimeters
+    focal_length = 3.60
+
+sensor_width = pixel_width * resolution_x
+sensor_height = pixel_width * resolution_y
 
 # dist_to_target - distance between lens and target (in mm)
 # size_of_target - one side of
@@ -19,9 +25,6 @@ def frame_image(sensor_width, sensor_height, dist_to_target, size_of_target):
     print(u, v)
     return ((1 - u) / 2, (1 - v) / 2, u, v)
 
-resolution_x = 1920
-resolution_y = 1080
-
-camera.resolution = (resolution_x, resolution_y)
+camera.resolution(resolution_x, resolution_y)
 camera.zoom = frame_image(sensor_width, sensor_height, DIST_FROM_LENSE_TO_GROUND, SIZE_OF_TARGET_ZONE)
 camera.capture('../foo.jpg')
