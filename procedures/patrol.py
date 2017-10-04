88c776bf7a05b425f6a253a46952ba5aa82a847f
Chris Chung <hiccupingminor@gmail.com>
2017-10-04 10:43:43 -0700

    Check if plant exists, and print output

--- procedures/patrol.py
+++ procedures/patrol.py
@@ -1,6 +1,6 @@
 from gcode import sendGcode
 import time
-# from camera.camera import square_has_plant
+from camera.camera import square_has_plant
 
 def go_home():
     sendGcode('G00 X0 Y0;\n')
@@ -9,9 +9,14 @@ def go_to(cell):
     # Send Gcode and wait for response
     sendGcode('G00 X' + str(cell[1]) + ' Y' + str(cell[2]) + ';\n')
 
+def process_cell(cell):
+    print("PLANT EXISTS:", square_has_plant())
+
 def patrol(grid):
     go_home()
     while grid.next_cell(): # Will return false if nothing left to go to
         grid.set_current_cell(grid.next_cell())
-        print(grid.get_current_cell())
-        go_to(grid.get_current_cell())
+        current_cell = grid.get_current_cell()
+        print(current_cell)
+        go_to(current_cell)
+        process_cell(current_cell)
