6f8a0fcbe9dfa9e583c407180caafa0ad15c3c7d
Chris Chung <hiccupingminor@gmail.com>
2017-10-04 21:25:32 -0700

    Add Cell construct

--- procedures/grid.py
+++ procedures/grid.py
@@ -7,24 +7,52 @@ CELL_SIZE = 10
 MAX_X = 330
 MAX_Y = 870
 
+class Cell():
+    def __init__(self, id, x, y, occupied, planted, watered):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.occupied = occupied
+        self.planted = planted
+        self.watered = watered
+
+        def _update():
+            c.execute("UPDATE cells SET occupied = (?), planted = (?), watered = (?) WHERE id = (?)", (self.occupied, self.planted, self.watered, self.id))
+
+        def set_planted(planted)
+            self.planted = planted
+            self._update()
+
+        def set_watered(watered):
+            self.watered = watered
+            self._update()
+
+        def set_occupied(occupied):
+            self.occupied = occupied
+            self._update()
+
+
 class Grid():
     def __init__(self):
         self.grid = []
         try:
             cells = c.execute("SELECT * FROM cells ORDER BY id ASC")
-            self.grid = cells.fetchall()
+            self.grid = map(lambda row: Cell(id=row.id,x=row.x,y=row.y,occupied=row.occupied,planted=row.planted,watered=row.watered), cells)
+
         except sqlite3.OperationalError:
             c.execute("CREATE TABLE cells (id integer primary key, x integer, y integer, occupied boolean, planted datetime, watered datetime)")
 
             cells = []
             # Based on boundaries, populate table with cells
+            id = 0
             for i in range(0, MAX_Y + CELL_SIZE, CELL_SIZE): # Must be inclusive
                 for j in range(0, MAX_X + CELL_SIZE, CELL_SIZE):
-                    cell = (j, i, False, None, None)
+                    id = id + 1
+                    cell = (id, j, i, False, None, None)
                     cells.append(cell)
-                    self.grid.append(cell)
+                    self.grid.append(Cell(id=id, x=cell[0], y=cell[1], occupied=cell[2], planted=cell[3], watered=cell[4]))
 
-            c.executemany("INSERT INTO cells (x, y, occupied, planted, watered) VALUES (?,?,?,?,?)", cells)
+            c.executemany("INSERT INTO cells (id, x, y, occupied, planted, watered) VALUES (?,?,?,?,?)", cells)
             conn.commit()
 
         self.current_cell = self.grid[0]
@@ -43,22 +71,22 @@ class Grid():
 
     def next_cell(self):
         current_cell = self.current_cell
-        is_advancing_row = ( current_cell[2] / 10 ) % 2 == 0
+        is_advancing_row = ( current_cell.y / 10 ) % 2 == 0
 
         if is_advancing_row:
-            if current_cell[1] == MAX_X:
-                print("AT MAX X", current_cell[1], current_cell[2] + CELL_SIZE)
-                return self.find_cell(x = current_cell[1], y = (current_cell[2] + CELL_SIZE)) # Move to next y row
+            if current_cell.x == MAX_X:
+                print("AT MAX X", current_cell.x, current_cell.y + CELL_SIZE)
+                return self.find_cell(x = current_cell.x, y = (current_cell.y + CELL_SIZE)) # Move to next y row
             else:
-                return self.find_cell(x = current_cell[1] + CELL_SIZE, y = current_cell[2])
+                return self.find_cell(x = current_cell.x + CELL_SIZE, y = current_cell.y)
         else:
-            if current_cell[1] == 0:
-                return self.find_cell(x = current_cell[1], y = current_cell[2] + CELL_SIZE)
+            if current_cell.x == 0:
+                return self.find_cell(x = current_cell.x, y = current_cell.y + CELL_SIZE)
             else:
-                return self.find_cell(x = current_cell[1] - CELL_SIZE, y = current_cell[2])
+                return self.find_cell(x = current_cell.x - CELL_SIZE, y = current_cell.y)
 
     def find_cell(self,x,y):
         for cell in self.grid:
-            if cell[1] == x and cell[2] == y:
+            if cell.x == x and cell.y == y:
                 return cell
         return False
